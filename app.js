eb01c5a84891711a5bb284d4dbbce6997c84a29c
ilariamotta <ilaria.motta93@gmail.com>
2025-12-16 15:19:26 +0100

    feat: generazione prima rotta

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    console.log("Server del mio blog")
+    res.json("Server del mio blog")
+})
+
+app.listen(port, () => {
+    console.log("Il server è in ascolto sulla porta " + port)
+})
+
